8a22f1a1addc27211d4fc0bcf42329fa4d2875a9
heybyben <heybyben@gmail.com>
2026-09-22 07:02:16 +0000

    kunzite: Add twrp-device-tree
    for missi-user 16 BP2A.250605.031.A3 16OS3.1.260825.205311778.QCPEGL.S release-keys
    
    Signed-off-by: heybyben <heybyben@gmail.com>

---
+++ twrp-device-tree/xiaomi/kunzite/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_kunzite.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_kunzite-user \
+    omni_kunzite-userdebug \
+    omni_kunzite-eng
